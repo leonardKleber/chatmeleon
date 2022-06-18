9a7dce84a0e0d4433b4c91196c79c9144c017730
Leonard Kleber <83468990+leonardKleber@users.noreply.github.com>
2022-06-19 00:58:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 from encryption import *
